42e5780c54fe0c4678973aaa3ab4fb94927d4879
tk373 <silva.hegner@gmail.com>
2024-10-31 09:11:58 +0100

    enviroment update

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -6,16 +6,21 @@ import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+const apiKey = import.meta.env.VITE_API_KEY;
+const senderId = import.meta.env.VITE_SENDER_ID;
+const appId = import.meta.env.VITE_APP_ID;
+const measurementId = import.meta.env.VITE_MEASUREMENT_ID;
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "REMOVED",
+  apiKey: apiKey,
   authDomain: "modul-vapp.firebaseapp.com",
   projectId: "modul-vapp",
   storageBucket: "modul-vapp.appspot.com",
-  messagingSenderId: "REMOVED",
-  appId: "REMOVED",
-  measurementId: "REMOVED"
+  messagingSenderId: senderId,
+  appId: appId,
+  measurementId: measurementId
 };
 
 // Initialize Firebase
